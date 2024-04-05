80b40214d751a4d4da6217d22f1d36f9cd06211b
Avinash <97391739+Avinash7390@users.noreply.github.com>
2024-04-05 22:17:05 +0530

    Update FrontEnd.js

--- Stripe/FrontEnd.js
+++ Stripe/FrontEnd.js
@@ -1,6 +1,6 @@
 const handlePayment = async () => {
   const stripe = await loadStripe(
-    "pk_test_51Oufb1SIR9oMWB8a1wc6gZSgOs3m4vTd6DYIup7jE5yyRky351W1nDEPAgzcmXqsuXaNqg1pfFlFall8OAHZZhKR00TpCXsACh"
+    "sk_test_51Oufb_wtfyoucopiedthiskeyaswellmanthisisjustsomedummykey"
   );
 
   if (paymentDone) {
